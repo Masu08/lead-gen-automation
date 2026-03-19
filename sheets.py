189314c4c7d00c88f1f7af67457b64700c8e1d4c
Masu08 <mallikakashyap32@gmail.com>
2026-03-19 19:52:35 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,1 +1,20 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+def save_to_sheets(data):
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/drive"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open("Leads").sheet1
+
+    for lead in data:
+        sheet.append_row([
+            lead.get("username"),
+            lead.get("bio"),
+            lead.get("followers"),
+            lead.get("engagement"),
+            lead.get("dm")
+        ])
